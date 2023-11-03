fcd57648f1b824061e9af63e23f970dfd7b6ce9c
Johan Yindou <jyindou@gmail.com>
2023-11-03 16:05:56 +0100

    Correctiondes liens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import React from "react";
-import { Link } from "react-router-dom";
 
 
 export default function Nav() {
@@ -48,31 +46,4 @@ export default function Nav() {
       </div>
     </nav>
   );
-
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-white py-3">
-            <div className="container px-5">
-                <Link to="/" className="navbar-brand">
-                    <span className="fw-bolder text-primary">Start Bootstrap</span>
-                </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/resume" className="nav-link">Resume</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/projects" className="nav-link">Projects</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/contact" className="nav-link">Contact</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
 }
